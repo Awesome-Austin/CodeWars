39671c2547b5824ddf4487060f687e56d8a94cbb
Awesome_Austin <{ID}+{username}@users.noreply.github.com>
2020-08-13 15:03:30 -0700

    Worked on Majong hand detector

--- Majong.py
+++ Majong.py
@@ -1,8 +1,5 @@
 #! python3
 
-from collections import Counter
-
-
 class Hand:
     def __init__(self, tiles):
         self._suits = {'simple': [9, 'psm'], 'honor': [7, 'z']}
@@ -17,10 +14,19 @@ class Hand:
             self.tiles[suit].append(n)
             self._all_tiles[suit].remove(n)
 
+    def _tiles_string(self, tiles):
+        return ''.join((f'{n}{s}' for s, ns in tiles.items() for n in ns))
+
+    def __repr__(self):
+        l = list()
+        for
+
+
+
+        return repr([(n, s) for s, ns in self.tiles.items() for n in ns])
+
     def __str__(self):
-        print([f'{s}{self.tiles[s]}' for n, suits in self._suits.values() for suit in suits for s in suit if s in self.tiles.keys()])
-
-
+        return self._tiles_string(self.tiles)
 
 
 if __name__ == '__main__':
@@ -28,4 +34,4 @@ if __name__ == '__main__':
     hand = Hand(tiles)
     # print(hand.tiles)
     # print(hand._all_tiles)
-    print(str(hand))
+    print(repr(hand))

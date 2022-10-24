5429792b7269f661493c7dd3781376f971648c0d
Awesome_Austin <12463568+Awesome-Austin@users.noreply.github.com>
2022-10-24 15:52:47 -0700

    refactor breadcrumb

--- BreadcrumbGenerator/main.py
+++ BreadcrumbGenerator/main.py
@@ -4,21 +4,19 @@ IGNORE = ['THE', 'OF', 'IN', 'FROM', 'BY', 'WITH', 'AND', 'OR', 'FOR', 'TO', 'AT
 
 
 def generate_bc(url, separator):
-    def acronym(crumb):
-        crumb = crumb.upper()
-        if crumb == '':
-            return 'HOME'
-        elif len(crumb) <= 30 or crumb.count('-') == 0:
-            return crumb.replace('-', ' ').split('.')[0]
-
-        return ''.join([word[0].split('.')[0] for word in crumb.split('-') if word not in IGNORE])
-
     def html_code(link_level, crumb, site_path):
-        if link_level == len(site_path) - 1:
-            return f'<span class="active">{acronym(crumb)}</span>'
-        return f'<a href="{"/".join(site[:(link_level + 1)] + [""])}">{acronym(crumb)}</a>'
+        acro = crumb.upper()
+        if acro == '':
+            acro = 'HOME'
+        elif len(acro) <= 30 or acro.count('-') == 0:
+            acro = acro.replace('-', ' ').split('.')[0]
+        else:
+            acro = ''.join([word[0].split('.')[0] for word in acro.split('-') if word not in IGNORE])
 
-    # print(f"('{url}', '{separator}')")
+        if link_level == len(site_path) - 1:
+            return f'<span class="active">{acro}</span>'
+        return f'<a href="{"/".join(site[:(link_level + 1)] + [""])}">{acro}</a>'
+
     site = [''] + urlsplit(url).path.split('/')[1:]
     if 'index' in site[-1].lower() or (len(site) > 1 and site[-1]) == '':
         site.pop()
